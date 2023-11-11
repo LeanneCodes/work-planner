b580daf5eca52dd4f6790d692140679d5ecfbb45
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-11 10:45:57 +0000

    identify the variables from the input IDs that need to be targeted

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,26 @@ var currentDay = $('#currentDay');
 currentDay.text(dayjs().format('dddd, MMMM D, YYYY'));
 
 // TODO: Present timeblocks for standard business hours when the user scrolls down.
-// time schedule should be from 9am to 5pm
-// if a block has the text content "lunch", highlight this a colour where no meetings can be scheduled
+// time schedule should be from 9am to 5pm ✅
 
 // TODO: Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 // if the current time is after a timeblock, that timeblock should be grey
 // if the current time matches a timeblock, it should be red
 // if the current time is before the remaining timeblocks, it should be green and available to add text
+
+var currentTime;
+var nineAM = $('input[id="9am"]');
+var tenAM = $('input[id="10am"]');
+var elevenAM = $('input[id="11am"]');
+var twelvePM = $('input[id="12pm"]');
+var onePM = $('input[id="1pm"]');
+var twoPM = $('input[id="2pm"]');
+var threePM = $('input[id="3pm"]');
+var fourPM = $('input[id="4pm"]');
+var fivePM = $('input[id="5pm"]');
+console.log(nineAM.val("hello"));
+console.log(tenAM.val("hello"));
+
 
 // TODO: Allow a user to enter an event when they click a timeblock
 // while the timeblock is green, the user can add text
